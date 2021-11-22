0d302d58b36d9eaf823234440125229099fdeb6d
Hoang Duy <duyctin2000@gmail.com>
2021-11-22 16:49:01 +0700

    add change password api

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -22,9 +22,27 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(max_length = 65, min_length = 8, write_only = True)
-    username = serializers.CharField(max_length = 255, min_length = 2)
+    password = serializers.CharField(max_length = 255, min_length = 1, write_only = True)
+    username = serializers.CharField(max_length = 255, min_length = 1)
 
     class Meta:
         model = User
         fields = ['username','password']
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length = 255, min_length = 2)
+    oldpassword = serializers.CharField(max_length = 255, min_length = 1, write_only = True)
+    newpassword = serializers.CharField(max_length = 65, min_length = 8, write_only = True)
+
+    class Meta:
+        model = User
+        fields = ['username','oldpassword','newpassword']
+    
+    def validate(self, attrs):
+        return super().validate(attrs)
+    
+    def create(self, data):
+        u = User.objects.get(username=data.get('username'))
+        u.set_password(data.get('newpassword'))
+        u.save()
+        return u
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
-from .serializers import UserSerializer,LoginSerializer
+from .serializers import ChangePasswordSerializer, UserSerializer,LoginSerializer
 
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,8 +16,22 @@ class RegisterView(GenericAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-        
 
+class ChangePasswordView(GenericAPIView):
+    serializer_class = ChangePasswordSerializer
+    def post(self, request):
+        data  = request.data
+        username = data.get('username','')
+        oldpassword = data.get('oldpassword','') 
+        user = auth.authenticate(username=username,password=oldpassword)
+        if user:
+            serializer = ChangePasswordSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response({'detail' :'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        
 class LoginView(GenericAPIView):
     serializer_class = LoginSerializer
     def post(self,request):
@@ -29,9 +43,7 @@ class LoginView(GenericAPIView):
         if user:
             
             auth_token = jwt.encode({'username':user.username},settings.JWT_SECRET_KEY)
-
-            serializer = UserSerializer(user)
-
+            
             data = {"token": auth_token}
             
             return Response(data, status=status.HTTP_200_OK)
